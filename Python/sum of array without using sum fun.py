7adafa3f4cd31035009b45d172a3bd405784fb9b
Prajwal Bharsakale <89308826+Prajwalbharsakale@users.noreply.github.com>
2026-06-12 02:40:53 +0000

    changes

---
+++ Python/sum of array without using sum fun.py
@@ -0,0 +1,20 @@
+# Sum of array elemenet with using sum function
+# a = [1,2,3,4,5,6]
+# print(sum(a));
+
+
+#without using sum function
+# a = [1,2,3,4,5,6]
+# test = 0
+# for i in range(0 , len(a)):
+#     test += a[i]
+
+# print(test);
+
+#without using sum function
+a = [1,2,3,4,5,6]
+test = 0
+for i in a:
+    test += i
+
+print(test);
